80551dbe9d73c6288bf62b9ce1fa7b56e7abe735
Stijn Volckaert <stijnv@gmail.com>
2023-01-02 16:24:43 +0100

    Deleted old project files

--- compile.bat
+++
@@ -1,6 +0,0 @@
-@cd..
-@del system\RMusicPlayer.u
-@system\ucc make ini=../RMusicPlayer/make.ini
-@copy system\RMusicPlayer.u rmusicplayer\release\_ready\RMusicPlayer.u
-@copy system\RMusicPlayer.dll rmusicplayer\release\_ready\RMusicPlayer.dll
-@pause
